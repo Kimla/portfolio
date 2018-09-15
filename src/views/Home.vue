63c1a84ceda76715c553276157bc388cce6fa1e7
Kim Larsson <kimla89@gmail.com>
2018-09-15 16:49:45 +0200

    Updated hero section and removed the rest.,

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,33 +1,19 @@
 <template>
     <div>
         <Hero></Hero>
-        <div class="cards">
-            <div class="container">
-                <Card></Card>
-                <Card></Card>
-                <Card></Card>
-                <Card></Card>
-            </div>
-        </div>
     </div>
 </template>
 
 <script>
 import Hero from '../components/Hero.vue';
-import Card from '../components/Card.vue';
 
 export default {
     name: 'home',
     components: {
         Hero,
-        Card,
     },
 };
 </script>
 
 <style lang="scss">
-.cards {
-    margin-top: 15vh;
-    padding-bottom: 50px;
-}
 </style>
